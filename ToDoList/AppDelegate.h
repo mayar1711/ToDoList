d6267cf158e28b4b5943585a91d6014261d3e069
mayar1711 <mayarhassan1711@gmail.com>
2024-04-02 09:31:18 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Mayar on 02/04/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ToDoList/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ToDoList
+//
+//  Created by Mayar on 02/04/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
